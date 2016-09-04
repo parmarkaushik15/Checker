270ef458acdfea52ed3dfd67839b0a4cad6ef0e8
kaushik <parmarkaushik15@gmail.com>
2016-09-04 19:02:27 +0530

    Checker Project

---
+++ src/Checker/Checkers.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Checker;
+
+/**
+ *
+ * @author Kaushik
+ */
+public class Checkers {
+    public static void main(String args[]){        
+        Game game = new Game();
+        game.PlayGame();        
+    }    
+}
